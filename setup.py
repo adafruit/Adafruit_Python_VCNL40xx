313084ea198ec834364f8283db30f3bc3248748d
Tony DiCola <tony@tonydicola.com>
2016-04-20 00:22:04 +0000

    Minor version bump to get consistent with pypi.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name              = 'Adafruit_VCNL40xx',
-      version           = '1.0.0',
+      version           = '1.0.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Python code to use the VCNL4000 & VCNL4010 proximity sensors with the Raspberry Pi & BeagleBone Black.',
